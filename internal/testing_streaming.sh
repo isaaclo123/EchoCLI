30c1fef690636ce6bc631b2e1c7b9a3f317839f3
Isaac Lo <isaaclo123@gmail.com>
2024-01-10 17:08:03 -0600

    streaming works; somewhat

--- internal/testing_streaming.sh
+++ internal/testing_streaming.sh
@@ -1,1 +1,2 @@
-nc 10.0.0.97 8080 | aplay --rate 16000 -B 2750 --period-size=512 -c 9
+#!/bin/bash
+nc 10.0.0.97 8080 | aplay -vvv --rate 16000 --period-size=2000 -c 9 -f S24_3LE --buffer-size 8000
--- shell/stream_audio.sh
+++ shell/stream_audio.sh
@@ -8,6 +8,8 @@ cd /data/local/tmp
 /data/local/tmp/busybox mkfifo /data/local/tmp/audio_fifo
 
 # tinycap file.wav [-D card] [-d device] [-c channels] [-r rate] [-b bits] [-p period_size] [-n n_periods] [-t duration] [-f]
+# tinymix | grep "Input Gain" | cut -f1
+
 killall tinycap
 killall mixer
 tinycap /data/local/tmp/audio_fifo -D 0 -d 24 -r 16000 -c 9 -b 24 -p 512 -n 5 -f &
