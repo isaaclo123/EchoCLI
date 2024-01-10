2fc5e98fe890aaf7a9694cd7c56eeffb1b354c50
Isaac Lo <isaaclo123@gmail.com>
2024-01-10 17:17:37 -0600

    audio streaming semi-works

--- internal/testing_streaming.sh
+++ internal/testing_streaming.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# IMPORTANT NOTE! For some reason, sometimes the audio is on 100% static all the time. Reopen the connection until it is okay.
+# Be careful of your ears, it can get really loud!
 nc 10.0.0.97 8080 | aplay -vvv --rate 16000 --period-size=2000 -c 9 -f S24_3LE --buffer-size 8000
